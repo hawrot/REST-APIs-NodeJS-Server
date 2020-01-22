4025eee6009a9a62c837169f656d0127189db2ef
hawrot <mhawro200@caledonian.ac.uk>
2020-01-22 21:13:46 +0100

    Fix CORS error

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*'); //access and domain
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE' ); //specify the methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
