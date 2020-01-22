8634de165b04436a3c132f029d98e44d761680d2
hawrot <mhawro200@caledonian.ac.uk>
2020-01-22 21:02:07 +0100

    Create post and get API function

---
+++ controllers/feed.js
@@ -0,0 +1,22 @@
+exports.getPosts = (req, res, next) =>{
+    res.status(200).json({
+        posts:{
+            title: 'First Post',
+            content: 'The first post content'
+        }
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+  //Create post in DB
+  res.status(201).json({
+      message: 'Post created!',
+      post: {
+          id: new Date().toISOString(),
+          title: title,
+          content: content
+      }
+  });
+};
