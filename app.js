faf865f72a19bfc2a37b929f12b93ded9a12b88c
hawrot <mhawro200@caledonian.ac.uk>
2020-01-29 15:54:52 +0000

    Connect to the DB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -15,5 +16,8 @@ app.use((req, res, next) =>{
 });
 
 app.use('/feed', feedRoutes);
-
-app.listen(8080);
+mongoose.connect('mongodb+srv://matt:ppULHJfdq6MjHqcV@cluster0-inhjh.mongodb.net/rest')
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
