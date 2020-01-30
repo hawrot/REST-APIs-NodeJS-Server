e63758b8c917aa97136a58b4f6f1981337a3ab44
hawrot <mhawro200@caledonian.ac.uk>
2020-01-30 14:10:19 +0000

    Delete the post

--- routes/feed.js
+++ routes/feed.js
@@ -20,6 +20,9 @@ router.put('/post/:postId',
     [
     body('title').trim().isLength({min: 5}),
         body('content').trim().isLength({min: 5})
-    ], feedController.updatePost);
+    ],
+    feedController.updatePost);
+
+router.delete('/post/:postId', feedController.deletePost);
 
 module.exports = router;
