7915dad2b20f105d30835cfcd8cbbb42100c803d
hawrot <mhawro200@caledonian.ac.uk>
2020-01-30 15:00:32 +0000

    Add validation inside routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const { body } = require('express-validator');
+const User = require('../models/user');
 
 const router = express.Router();
+const authController = require('../controllers/auth');
 
-router.put('/signup');
+
+router.put('/signup', [
+    body('email').isEmail().withMessage('Enter valid Emial').custom((value, {req}) =>{
+        return User.findOne({email: value}).then(userDoc => {
+            if(userDoc){
+                return Promise.reject('Email exists');
+            }
+        })
+    }).normalizeEmail(),
+    body('password').trim().isLength({min : 5}),
+    body('name').trim().not().isEmpty()
+
+], authController.signup);
+
+
 
 module.exports = router;
