fce53bfba78b576a8bc27afe4f39195d222c9540
hawrot <mhawro200@caledonian.ac.uk>
2020-01-30 14:45:36 +0000

    Add pagination

--- models/post.js
+++ models/post.js
@@ -1,24 +1,24 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    title:{
-        type: String,
-        required: true
+        title: {
+            type: String,
+            required: true
+        },
+        imageUrl: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        creator: {
+            type: Object,
+            required: String
+        }
     },
-    imageUrl:{
-        type: String,
-        required: true
-    },
-    content:{
-        type: String,
-        required: true
-    },
-    creator:{
-        type: Object,
-        required: String
-    }
-},
     {timestamps: true}
 );
 
