ccfa8ecf75160ee7e60f3f575ddae7c6d844934d
hawrot <mhawro200@caledonian.ac.uk>
2020-02-02 23:07:51 +0000

    Add log in auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator/check');
+const jwt = require('jsonwebtoken');
 
 const bcrypt = require('bcryptjs');
 
@@ -55,6 +56,9 @@ exports.login = (req, res, next) =>{
                 error.statusCode = 401;
                 throw error;
             }
+            const token = jwt.sign({email: loadedUser.email, userId: loadedUser._id.toString()}, 'supersecret', {expiresIn: '1h'});
+            res.status(200).json({token: token, userId : loadedUser._id.toString()});
+
 
         })
         .catch(err =>{
